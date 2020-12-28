00c1c9e60f5ef11d6b9b101e2febc460f7edfe72
huangzhixue <1104428690@qq.com>
2020-12-28 17:45:22 +0800

    JDK1.8源码编译并通过测试，交由github托管

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+/**
+ * 测试编译是否成功
+ *
+ * @author ：HUANG ZHI XUE
+ * @date ：Create in 2020-12-28
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Build Success!");
+        HashMap hashMap = new HashMap();
+    }
+}
